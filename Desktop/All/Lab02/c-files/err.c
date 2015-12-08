f2d9befd161679d70b5b90b72b05e6a8e1756c35
Kyle Rakos <krakos@purdue.edu>
2015-12-08 16:44:18 -0500

    removed Desktop

--- Desktop/All/Lab02/c-files/err.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main ()
-{
-  printf ("Running file err.c\n");
-  return 3.145;
-}
--- Desktop/All/Lab11/topy.bash
+++
@@ -1,2 +0,0 @@
-python3.4 /opt/python3/current/lib/python3.4/site-packages/PySide/scripts/uic.py HomographyGUI.ui -o HomographyGUI.py
-
